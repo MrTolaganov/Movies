64044b6aacc0229708e8bf9b4b540ae2a066fe21
Otabek <otabektolaganov69@gmail.com>
2023-12-13 10:23:44 +0500

    first-commit

---
+++ src/components/app/app.js
@@ -0,0 +1,29 @@
+import "./app.css";
+import AppFilter from "../app-filter/app-filter";
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import MovieList from "../movie-list/movie-list";
+import MoviesAddForm from "../movies-add-form/movies-add-form";
+
+const App = () => {
+  const data = [
+    { name: "Empire of osman", viewers: 988, favourite: false, id: 1 },
+    { name: "Ertugrul", viewers: 789, favourite: false, id: 2 },
+    { name: "Osman", viewers: 1091, favourite: true, id: 3 },
+  ];
+  return (
+    <div className="app font-monospace">
+      <div className="content">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <MovieList data={data} />
+        <MoviesAddForm />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/movie-list/movie-list.js
@@ -0,0 +1,19 @@
+import MovieListItem from "../movie-list-item/movie-list-item";
+import "./movie-list.css";
+
+const MovieList = ({ data }) => {
+  return (
+    <ul className="movie-list">
+      {data.map((item) => (
+        <MovieListItem
+          key={item.id}
+          name={item.name}
+          viewers={item.viewers}
+          favourite={item.favourite}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
